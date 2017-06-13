dda259928802b9c382c503b789ac5aacae3391b0
Ludwik Bukowski <ludwik.bukowski@gmail.com>
2017-06-14 01:28:08 +0200

    init project

---
+++ src/main/java/Main.java
@@ -0,0 +1,14 @@
+/**
+ * Created by ludwikbukowski on 14/06/17.
+ */
+import org.openvpp.jvpp.*;
+
+import java.io.IOException;
+
+public class Main {
+    public static void main(String[] args) throws IOException {
+        System.out.println("starting ");
+
+
+    }
+}
